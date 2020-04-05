3022349a964c90b3212b527b321b274ae50c0aef
Naheed Khan <naheed.khan@somaiya.edu>
2020-04-05 19:52:31 +0530

    magic commit

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-
+<center>
+<h1>Enjoy Learning</h1></center>
+<form ></form>
+<div id=class>
 @foreach($data as $data)
-<video width="320" height="240" controls>
+<input type="hidden" value="{{asset($data->url)}}">
+<center><video width="auto" height="auto" controls>
   <source src="{{asset($data->url)}}" type="video/mp4">
   Your browser does not support the video tag.
-</video>
+</video></center>
 @endforeach
-
+<div>
+<center>
+<footer>copy@rights</footer>
+</center>
+</form>
+<script src="{{asset('js/video.js')}}"></script>
 @endsection
